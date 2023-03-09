e85c7ed3ec9be092e2c6c942fa9f5ac0070169ea
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-09 10:53:23 +0300

    add MemoryChannelLayer

--- WebSocketChat/WebSocketChat/settings.py
+++ WebSocketChat/WebSocketChat/settings.py
@@ -98,12 +98,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = "WebSocketChat.asgi.application"
 
 
+# CHANNEL_LAYERS = {
+#     'default':   {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             'hosts': [('127.0.0.1', 6379)]
+#         },
+#     }
+# }
+
 CHANNEL_LAYERS = {
-    'default':   {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)]
-        },
+    'default': {
+        'BACKEND': "channels.layers.InMemoryChannelLayer"
     }
 }
 

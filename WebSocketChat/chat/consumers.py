a2dca47ca8543e1a0227a2e98bec4f2d8e35d4d0
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-09 14:09:37 +0300

    consumers.py saves data in DB

--- WebSocketChat/chat/consumers.py
+++ WebSocketChat/chat/consumers.py
@@ -1,20 +1,19 @@
 import json
-from datetime import datetime
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+
+from .models import Message
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
 
     def disconnect(self, close_code):
@@ -28,22 +27,29 @@ class ChatConsumer(WebsocketConsumer):
         message = text_data_json['message']
         username = text_data_json['username']
 
+        save_message = Message.objects.create(
+            message=message,
+            username=username
+        )
+
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'username': username
+                'username': username,
+                'pub_date': save_message.pub_date
             }
         )
 
     def chat_message(self, event):
         message = event['message']
         username = event['username']
-        pub_date = datetime.now()
+        pub_date = event['pub_date']
+
         self.send(text_data=json.dumps({
             'event': "Send",
             'message': message,
             'username': username,
-            'pub_date': str(pub_date.strftime('%H:%M')),
+            'pub_date': str(pub_date),
         }))

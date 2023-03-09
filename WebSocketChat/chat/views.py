9dadda52841d6059cceac606dbaa8cb74391a2c5
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-09 14:10:19 +0300

    small fixes

--- WebSocketChat/chat/views.py
+++ WebSocketChat/chat/views.py
@@ -1,15 +1,22 @@
 import json
 
+from django.core import serializers
 from django.shortcuts import render
 from django.utils.safestring import  mark_safe
 
+from .models import Message
 
 def index(request):
     return render(request, 'chat/index.html')
 
 
 def room(request, room_name):
+
+    messages = Message.objects.all()
+    data = serializers.serialize("json", messages)
+
     context = {
-        'room_name': room_name
+        'room_name': room_name,
+        'messages': data,
     }
     return render(request, 'chat/room.html', context)

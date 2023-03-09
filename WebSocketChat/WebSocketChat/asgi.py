3bb9eff3acef7e3927e171298d62b3c907d31f9a
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-09 09:11:25 +0300

    add root routing configuration

--- WebSocketChat/WebSocketChat/asgi.py
+++ WebSocketChat/WebSocketChat/asgi.py
@@ -1,12 +1,21 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from .. import chat
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WebSocketChat.settings")
 
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        )
     }
 )

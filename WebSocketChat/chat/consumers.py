511d0dd0772fa7a4c3a42aca39393c3832ad7988
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-03-09 10:52:29 +0300

    add async func to send message

--- WebSocketChat/chat/consumers.py
+++ WebSocketChat/chat/consumers.py
@@ -1,18 +1,49 @@
 import json
+from datetime import datetime
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = text_data_json['username']
 
-        self.send(text_data=json.dumps({'message': message}))
-    
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        pub_date = datetime.now()
+        self.send(text_data=json.dumps({
+            'event': "Send",
+            'message': message,
+            'username': username,
+            'pub_date': str(pub_date.strftime('%H:%M')),
+        }))
